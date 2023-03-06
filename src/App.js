17317e8c9b4800cda4eb9bbbf93e1a0397e5f77a
Monique Azevedo <monique.azevedo@grupoboticario.com.br>
2023-03-06 09:06:02 -0300

    cria e importa header e meu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import Button from './components/Button'
-
+import Header from './components/Header'
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
